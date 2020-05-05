ea0f9cc7b4a1cfab83c97b34496d3c8040b9bf65
Abinaya0896 <abiabiaya834@gmail.com>
2020-05-05 05:54:37 +0530

    Suji update the code

--- src/test/java/test/Repo1.java
+++ src/test/java/test/Repo1.java
@@ -5,9 +5,9 @@ public class Repo1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Abi");
-		System.out.println("Abinaya");
+		System.out.println("Thenmozhi");
 		
 		System.out.println("Amutha");
-		
+		System.out.println("Prathisuji");
 	}
 }
